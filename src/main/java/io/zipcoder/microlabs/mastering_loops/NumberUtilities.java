b6df47e74b173a439ca5eebc6410f9b421892b80
Chanelle <chanelle@Zipcoders-MacBook-Pro-103.local>
2023-07-07 13:27:18 -0400

    Submitting project NumbersTrianglesTables

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -1,75 +1,75 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 
-import java.util.ArrayList;
-
 public class NumberUtilities {
-    public static String getEvenNumbers(int start, int stop) {
-        String chanelle = "";
-        for (int i = start; i < stop; i++) {
-            if (i % 2 != 0) {
-        chanelle += i;
-            }
-        }
-        return chanelle;
+    public static boolean isOdd(int n) {
+        return (n % 2 != 0);
     }
 
-    public boolean isEven(int n) {
-        if (n % 2 == 0){
-            return true;
+    public static String getEvenNumbers(int start, int stop) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = start; i < stop; i++) {
+            if (isOdd(i)) {
+                sb.append(i);
+            }
         }
-        return false;
-     }
+        return sb.toString();
+    }
 
 
     public static String getOddNumbers(int start, int stop) {
-        String oddNumbers = "";
+        String result = "";
+
         for (int i = start; i < stop; i++) {
-            if (i % 2 == 0) {
-                oddNumbers += i;
-                //declared a variable called odd numbers
-                //created a loop to hit start and stop
+            if (!isOdd(i)) {
+                result = result + i;
             }
         }
-        return oddNumbers;
+        return result;
     }
+    // String oddNumbers = "";
+    // for (int i = start; i < stop; i++) {
+    //  if (i % 2 == 0) {
+    // oddNumbers += i;
+    // declared a variable called odd numbers
+    // created a loop to hit start and stop
+    // return oddNumbers;
 
-
-    public static String getSquareNumbers(int start, int stop, int step) {
-        String squareNumbers = "";
-        for (int i = start; i < stop; i=i+step) {
-            squareNumbers += i*i;
-        }
-        return squareNumbers;
-        // i*i is multiplying the number by itself
-        // i=i+step, how to get from the start to the stop
-    }
 
     public static String getRange(int stop) {
-        return getRange(0, stop);
+        return getRange(0, stop, 1);
+        // return getRange(0, stop);
+        // different methods with minor differences
+        // cleaner code w/ using method overload
     }
-    // different methods with minor differences
-    // cleaner code w/ using method overload
+
     public static String getRange(int start, int stop) {
-        return getRange(0, stop,1);
+        return getRange(start, stop, 1);
+        // return getRange(0, stop,1);
+        // step 1 b/c you want every number to go by 1 bc it was not specified
     }
-// step 1 b/c you want every number to go by 1 bc it was not specified
 
     public static String getRange(int start, int stop, int step) {
-        String range = "";
-        for (int i = start; i < stop; i=i+step) {
-            range += i;
-        }
-        return range;
+        return getExponentiation(start, stop, step, 1);
+        // String range = "";
+        // for (int i = start; i < stop; i=i+step) {
+        // range += i;
+        //return range;
+    }
+    public static String getSquareNumbers(int start, int stop, int step) {
+        return getExponentiation(start, stop, step, 2);
+
     }
 
+    public static String getExponentiation(int start, int stop, int step, int exponent) {
 
-    public static String getExponentiations(int start, int stop, int step, int exponent) {
-        String range = "";
-        for (int i = start; i < stop; i=i+step) {
-            range += i;
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = start; i < stop; i += step) {
+                // for loop which multiplied i 'exponent' times
+                sb.append((int) Math.pow(i, exponent));
+            }
+            return sb.toString();
         }
-        return null;
     }
-}
-//Unsure how to add an exponent
